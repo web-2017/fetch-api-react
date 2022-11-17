114378311fda47c201c0cf1415efc4bc97976229
Mario Magomedov <webdevelope2017@gmail.com>
2022-11-17 09:55:59 -0600

    postApi

--- src/FetchApi/FetchApi.ts
+++ src/FetchApi/FetchApi.ts
@@ -8,32 +8,82 @@ export interface IFetchApiProps {
 export interface IFetchByIdApiProps extends IFetchApiProps {
 	id: string
 }
+export interface IFetchPostProps extends IFetchApiProps {
+	body: object
+}
+
+const defaultHeaders = { 'Content-type': 'application/json; charset=UTF-8' }
 
 class FetchApi {
+	seeAllMethods() {
+		return console.info(`
+			getApi({ url, headers })
+			getByIdApi({ url, id, headers })
+		`)
+	}
 	getApi({ url, headers }: IFetchApiProps) {
 		if (!url) {
 			console.log('Url is required')
 			return
 		}
-		return fetch(url, { method: 'GET', headers: { ...headers } })
-			.then((docJson) => docJson.json())
-			.then((data) => data)
-			.catch((error) => console.error(error))
+		return fetch(url, {
+			method: 'GET',
+			headers: headers ? { ...headers } : defaultHeaders,
+		})
+			.then((docJson: any) => docJson.json())
+			.then((data: any) => data)
+			.catch((error: any) => console.error(error))
 	}
 	getByIdApi({ url, id, headers }: IFetchByIdApiProps) {
 		if (!url) {
-			console.log('Url is required!!!')
+			console.log(
+				'Url is required!!!, Example: getByIdApi({ url: "http://google.com" }'
+			)
 			return
 		}
 		if (!id) {
-			console.log('Id is required!!!')
+			console.log('Id is required!!!, Example: getByIdApi({ id: 1 }')
 			return
 		}
-		return fetch(`${url}/${id}`, { method: 'GET', headers: { ...headers } })
-			.then((docJson) => docJson.json())
-			.then((data) => data)
-			.catch((error) => console.error(error))
+		return fetch(`${url}/${id}`, {
+			method: 'GET',
+			headers: headers ? { ...headers } : defaultHeaders,
+		})
+			.then((docJson: any) => docJson.json())
+			.then((data: any) => data)
+			.catch((error: any) => console.error(error))
+	}
+	postApi({ url, body, headers }: IFetchPostProps) {
+		if (!url) {
+			console.log(
+				'Url is required!!!, Example: getByIdApi({ url: "http://google.com" }'
+			)
+			return
+		}
+
+		if (typeof body !== 'object') {
+			console.log(
+				'body must be an object!!!, Example: body: { title: "Hello world", firstName: John}'
+			)
+			return
+		}
+		if (!body) {
+			console.log(
+				'body object is required!!!, Example: getByIdApi({ body: {title: hello} }'
+			)
+			return
+		}
+		return fetch(`${url}`, {
+			method: 'POST',
+			headers: headers ? { ...headers } : defaultHeaders,
+			body: JSON.stringify({ ...body }),
+		})
+			.then((docJson: any) => docJson.json())
+			.then((data: any) => data)
+			.catch((error: any) => console.error(error))
 	}
 }
 
-export default FetchApi
+const api = new FetchApi()
+
+export default api
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
-import FetchApi from './FetchApi/FetchApi'
-
-const api = new FetchApi()
+import api from './FetchApi/FetchApi'
 
 const getApi = api.getApi
 const getByIdApi = api.getByIdApi
+const seeAllMethods = api.seeAllMethods
 
-export { getApi, getByIdApi }
+export { getApi, getByIdApi, api, seeAllMethods }
